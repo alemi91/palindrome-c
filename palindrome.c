96ec33d74048f1c6e239e35df03e23925b41949e
alemi91 <amihalache2006@gmail.com>
2026-06-20 12:21:09 +0300

    update code

--- palindrome.c
+++ palindrome.c
@@ -1,25 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int oglind(int nr) {
-	int aux = 0;
-	while (nr != 0) {
-		aux = aux * 10 + nr % 10;
-		nr /= 10;
-	}
-	return aux;
+    int aux = 0;
+    while (nr != 0) {
+        aux = aux * 10 + nr % 10;
+        nr /= 10;
+    }
+    return aux;
+}
+
+void sort(int *palindrome, int nrPali) {
+	for (int i = 0; i < nrPali - 1; i++) {
+        for (int j = i + 1; j < nrPali; j++) {
+            if (palindrome[i] > palindrome[j]) {
+                int aux = palindrome[i];
+                palindrome[i] = palindrome[j];
+                palindrome[j] = aux;
+            }
+        }
+    }
 }
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		int nr;
-		scanf("%d", &nr);
-		if (nr == oglind(nr)) {
-			printf("%d is a palindrome!\n", nr);
-		} else {
-			printf("%d is NOT a palindrome!\n", nr);
-		}
-	}
+    int n;
+    printf("How many numbers? ");
+    scanf("%d", &n);
+
+    int *palindrome = calloc(n, sizeof(int)); 
+    int nrPali = 0;
+
+    for (int i = 0; i < n; i++) {
+        int nr;
+        printf("\n%d. ", i + 1);
+        scanf("%d", &nr);
+        
+        if (nr == oglind(nr)) {
+            printf("\n%d is a palindrome!", nr);
+            palindrome[nrPali] = nr;
+            nrPali++;
+        } else {
+            printf("\n%d is NOT a palindrome!", nr);
+        }
+    }
+
+    sort(palindrome, nrPali);
+
+    printf("\nPalindromes found: %d\n", nrPali);
+    
+    if (nrPali > 0) {
+        for (int i = 0; i < nrPali; i++) {
+            printf("%d ", palindrome[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
